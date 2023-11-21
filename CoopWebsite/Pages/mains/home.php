40afa92d54485d6855a1421c8cd4d53b8b1b3571
Jullian Ray <119770018+AWheat1@users.noreply.github.com>
2023-11-21 13:18:08 +0800

    Add files via upload

---
+++ CoopWebsite/Pages/mains/home.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="icon" href="/Assets/qculogo.png">
+    <link rel="stylesheet" href="../styles/home.css">
+    <title>QCUStore</title>
+</head>
+<body>
+  <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #ff525f;" >
+        <div class="container">
+          <a class="navbar-brand" href="#" style="color: #1982c4;"><img src="../Assets/qcu-logo2.png" alt="" width="40" height="40" class="">
+            <span style="color: #ffca3a;">QCU</span>Store</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Product</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Orders</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../mains/login.html">Account</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+<!-- HOME -->
+<div id="carouselSlides" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div id="websiteName" class="position-absolute top-50 start-0 translate-middle-y p-3">
+        <h1><span style="font-size: 64px; font-weight: bolder; color: #ffca3a;">QCU</span>Store</h1>
+        <p>Welcome to QCUStore where you can now buy items 
+            <br>in our COOP through online with the help of SBIT-2E.</p>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="3000">
+        <img src="../Assets/qcu-image-01.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+        <img src="../Assets/qcu-image-02.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+        <img src="../Assets/qcu-image-03.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+</div>
+<!--  -->
+<section style="background-color: whitesmoke;">
+    <div class="container py-5">
+      <div class="row">
+<!-- CARD -->
+        <div class="col-md-12 col-lg-4 mb-4 mb-lg-0">
+          <div class="card">
+            <a href="#" style="text-decoration: none; color: black;">
+            <div class="d-flex justify-content-between p-3">
+              <p class="lead mb-0">Classic QCU Uniform</p>
+            </div>
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/4.webp"
+              class="card-img-top" alt="Laptop" />
+            <div class="card-body">
+              <div class="d-flex justify-content-between mb-3">
+                <h5 class="mb-0">Polo</h5>
+                <h5 class="text-dark mb-0">₱#</h5>
+              </div>
+              <div class="d-flex justify-content-between mb-2">
+                <p class="text-muted mb-0">Available: <span class="fw-bold">#</span></p>
+              </div>
+            </div>
+          </div>
+            </a>
+        </div>
+<!-- CARD -->
+        <div class="col-md-6 col-lg-4 mb-4 mb-md-0">
+          <div class="card">
+            <a href="#" style="text-decoration: none; color: black;">
+            <div class="d-flex justify-content-between p-3">
+              <p class="lead mb-0">Classic QCU Uniform</p>
+            </div>
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/7.webp"
+              class="card-img-top" alt="Laptop" />
+            <div class="card-body">
+              <div class="d-flex justify-content-between mb-3">
+                <h5 class="mb-0">Pants</h5>
+                <h5 class="text-dark mb-0">₱#</h5>
+              </div>
+              <div class="d-flex justify-content-between mb-2">
+                <p class="text-muted mb-0">Available: <span class="fw-bold">#</span></p>
+              </div>
+            </div>
+          </div>
+            </a>
+        </div>
+<!-- CARD -->
+        <div class="col-md-6 col-lg-4 mb-4 mb-md-0">
+          <div class="card">
+            <a href="#" style="text-decoration: none; color: black;">
+            <div class="d-flex justify-content-between p-3">
+              <p class="lead mb-0">Today's Combo Offer</p>
+            </div>
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/5.webp"
+              class="card-img-top" alt="Gaming Laptop" />
+            <div class="card-body">
+              <div class="d-flex justify-content-between mb-3">
+                <h5 class="mb-0">QCU ID Lace</h5>
+                <h5 class="text-dark mb-0">₱#</h5>
+              </div>
+              <div class="d-flex justify-content-between mb-2">
+                <p class="text-muted mb-0">Available: <span class="fw-bold">#</span></p>
+              </div>
+            </div>
+          </div>
+            </a>
+        </div>
+<dir></dir>
+<!-- CARD -->
+<div class="col-md-12 col-lg-4 mb-4 mb-lg-0">
+  <div class="card">
+    <a href="#" style="text-decoration: none; color: black;">
+    <div class="d-flex justify-content-between p-3">
+      <p class="lead mb-0">Today's Combo Offer</p>
+    </div>
+    <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/4.webp"
+      class="card-img-top" alt="Laptop" />
+    <div class="card-body">
+      <div class="d-flex justify-content-between mb-3">
+        <h5 class="mb-0">Classic Uniform</h5>
+        <h5 class="text-dark mb-0">₱#</h5>
+      </div>
+      <div class="d-flex justify-content-between mb-2">
+        <p class="text-muted mb-0">Available: <span class="fw-bold">#</span></p>
+      </div>
+    </div>
+  </div>
+    </a>
+</div>
+<!-- CARD -->
+<div class="col-md-12 col-lg-4 mb-4 mb-lg-0">
+  <div class="card">
+    <a href="#" style="text-decoration: none; color: black;">
+    <div class="d-flex justify-content-between p-3">
+      <p class="lead mb-0">Today's Combo Offer</p>
+    </div>
+    <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/4.webp"
+      class="card-img-top" alt="Laptop" />
+    <div class="card-body">
+      <div class="d-flex justify-content-between mb-3">
+        <h5 class="mb-0">Classic Uniform</h5>
+        <h5 class="text-dark mb-0">₱#</h5>
+      </div>
+      <div class="d-flex justify-content-between mb-2">
+        <p class="text-muted mb-0">Available: <span class="fw-bold">#</span></p>
+      </div>
+    </div>
+  </div>
+    </a>
+</div>
+<!-- CARD -->
+<div class="col-md-12 col-lg-4 mb-4 mb-lg-0">
+  <div class="card">
+    <a href="#" style="text-decoration: none; color: black;">
+    <div class="d-flex justify-content-between p-3">
+      <p class="lead mb-0">Today's Combo Offer</p>
+    </div>
+    <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/4.webp"
+      class="card-img-top" alt="Laptop" />
+    <div class="card-body">
+      <div class="d-flex justify-content-between mb-3">
+        <h5 class="mb-0">Classic Uniform</h5>
+        <h5 class="text-dark mb-0">₱#</h5>
+      </div>
+      <div class="d-flex justify-content-between mb-2">
+        <p class="text-muted mb-0">Available: <span class="fw-bold">#</span></p>
+      </div>
+    </div>
+  </div>
+    </a>
+</div>
+      </div>
+    </div>
+  </section>
+
+<!-- Dapat nakalagay na to sa footer file -->
+<footer class=" text-center text-white" style="background-color: #ff525f">
+  <div class="container p-4 pb-0">
+    <section class="mb-4">
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+        <i class='bx bxl-facebook'></i>
+      </a>
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+        <i class='bx bxl-twitter' ></i>
+      </a>
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+        <i class='bx bxl-instagram-alt' ></i>
+      </a>
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+        <i class='bx bxl-tiktok' ></i>
+      </a>
+    </section>
+  </div>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Copyright:
+    <a class="text-white" href="#">QCUStore.com</a>
+  </div>
+</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ CoopWebsite/Pages/scripts/login.js
@@ -0,0 +1,28 @@
+const home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("ri-eye-off-line", "ri-eye-line");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("ri-eye-line", "ri-eye-off-line");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
